2894c1fb1d908bb9941132c11afba536dbca6fd9
wheatgreaser <wheatgreaser3000@gmail.com>
2024-12-01 10:52:04 +0530

    yoo

--- lexification.cpp
+++ lexification.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cctype>
 
 enum TokenType
 {
@@ -9,9 +10,8 @@ enum TokenType
     OpenParen,
     CloseParen,
     BinaryOperator,
-    Set,
+    Set
 };
-
 
 struct Token
 {
@@ -19,17 +19,21 @@ struct Token
     TokenType type;
 };
 
-
-std::vector<std::string> splitString(const std::string &sourceCode){
+std::vector<std::string> splitString(const std::string &sourceCode)
+{
     std::vector<std::string> words;
     std::string word;
-    for (int i = 0; i < sourceCode.length(); i++){
-        if (sourceCode[i] != ' '){
-            word = word + sourceCode[i];
+    for (int i = 0; i < sourceCode.length(); i++)
+    {
+        if (sourceCode[i] != ' ')
+        {
+            word += sourceCode[i];
         }
-        
-        if (sourceCode[i] == ' ' || i == sourceCode.length() - 1) {
-            if (!word.empty()) {
+
+        if (sourceCode[i] == ' ' || i == sourceCode.length() - 1)
+        {
+            if (!word.empty())
+            {
                 words.push_back(word);
                 word.clear();
             }
@@ -38,20 +42,56 @@ std::vector<std::string> splitString(const std::string &sourceCode){
     return words;
 }
 
-std::vector<Token> tokenize(std::string &sourceCode) {
+std::vector<Token> tokenize(std::string &sourceCode)
+{
     std::vector<Token> tokens;
     std::vector<std::string> src = splitString(sourceCode);
 
-    for (int i = 0; i < src.size(); i++){
-        std::cout << src[i] << " ";
+    for (const std::string &word : src)
+    {
+        if (std::isdigit(word[0]))
+        {
+            tokens.push_back({word, TokenType::Number});
+        }
+        else if (word == "=")
+        {
+            tokens.push_back({word, TokenType::Equals});
+        }
+        else if (word == "(")
+        {
+            tokens.push_back({word, TokenType::OpenParen});
+        }
+        else if (word == ")")
+        {
+            tokens.push_back({word, TokenType::CloseParen});
+        }
+        else if (word == "+" || word == "-" || word == "*" || word == "/")
+        {
+            tokens.push_back({word, TokenType::BinaryOperator});
+        }
+        else
+        {
+            tokens.push_back({word, TokenType::Identifier});
+        }
     }
+
     return tokens;
 }
 
+void print_stuff(std::vector<Token> &tokens)
+{
+    for (const Token &token : tokens)
+    {
+        std::cout << "Token(Value: " << token.value << ", Type: " << token.type << ")\n";
+    }
+}
 
-
-
-int main(){
-    std::cout << "wassgood cuh" << " ";
+int main()
+{
+    std::string s;
+    std::cout << "Enter your source code: ";
+    std::getline(std::cin, s);
+    std::vector<Token> tokens = tokenize(s);
+    print_stuff(tokens);
     return 0;
 }
